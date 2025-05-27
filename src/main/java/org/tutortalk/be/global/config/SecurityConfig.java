cd48500b6a55241aa1f85c51d2f53bd46205b14f
Jhwan02 <shinjh0218@gmail.com>
2025-05-28 06:35:42 +0900

    FEAT: OAUTH 설정 추가 및 CORS 설정 추가

--- src/main/java/org/tutortalk/be/global/config/SecurityConfig.java
+++ src/main/java/org/tutortalk/be/global/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package org.tutortalk.be.global.config;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -12,6 +13,9 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.tutortalk.be.global.oauth.GoogleOAuth2SuccessHandler;
 import org.tutortalk.be.global.util.CustomUserDetailsService;
 
 @Configuration // 설정 클래스
@@ -20,6 +24,7 @@ import org.tutortalk.be.global.util.CustomUserDetailsService;
 public class SecurityConfig {
     private final JwtAuthenticationFilter jwtAuthenticationFilter;
     private final CustomUserDetailsService userDetailsService;
+    private final GoogleOAuth2SuccessHandler googleOAuth2SuccessHandler;
 
     /*
      * 사용자 비밀번호를 안전하게 암호화하기 위하여
@@ -46,13 +51,30 @@ public class SecurityConfig {
                 .csrf(csrf -> csrf.disable()) // JWT는 CSRF 필요 없으므로 꺼주기
                 .sessionManagement(session->session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
-                                .requestMatchers("/api/auth/**").permitAll() // 회원가입, 로그인은 인증 불필요
+                                .requestMatchers(HttpMethod.POST, "/api/auth/**").permitAll()
+                                .requestMatchers("/","/oauth2/**").permitAll()
                                 .anyRequest().authenticated() // 나머지 요청은 인증 필요
-                        //.anyRequest().permitAll() 모든 요청 허용
                 )
+                .formLogin(form -> form.disable()) // 로그인 폼 비활성화 (나중에 지워줘야 할 코드)
                 .userDetailsService(userDetailsService)
-                .formLogin(login -> login.disable()) // 로그인 폼 비활성화 (나중에 지워줘야 할 코드)
                 .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
+                .oauth2Login(oauth2 -> oauth2
+                        .successHandler(googleOAuth2SuccessHandler) // oauth로그인이 성공했을 경우 실행할 클래스
+                )
                 .build();
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("http://localhost:3000")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH")
+                        .allowedHeaders("*")
+                        .allowCredentials(true); // 인증정보 (쿠키, 인증 헤더) 허용
+            }
+        };
     }
 }
